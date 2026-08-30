b6910fd85451aa3d39f02af6699d6b45e61f38b8
Anjum Asiya <anjumasiya613@gmail.com>
2026-08-30 21:26:22 +1000

    feat: add fail-closed HTTPX scientific backend

--- src/cmpilot/final_runtime_backends.py
+++ src/cmpilot/final_runtime_backends.py
@@ -13,6 +13,7 @@ from .final_model_runtime import (
     build_qwen32b_model_executor,
 )
 from .final_runner import ModelExecutor, ScientificOperations
+from .httpx_backend import HTTPX_BACKEND_ID, build_httpx_backend
 from .mcp_pinot_backend import MCP_PINOT_BACKEND_ID, build_mcp_pinot_backend
 
 
@@ -21,7 +22,10 @@ ModelExecutorBuilder = Callable[[Mapping[str, Any]], ModelExecutor]
 
 
 SCIENTIFIC_BACKENDS: Mapping[str, ScientificBackendBuilder] = MappingProxyType(
-    {MCP_PINOT_BACKEND_ID: build_mcp_pinot_backend}
+    {
+        HTTPX_BACKEND_ID: build_httpx_backend,
+        MCP_PINOT_BACKEND_ID: build_mcp_pinot_backend,
+    }
 )
 MODEL_EXECUTORS: Mapping[str, ModelExecutorBuilder] = MappingProxyType(
     {
